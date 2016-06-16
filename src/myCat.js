c5ab73f890e02ddd8f153863b0225923b32c53dc
Oleg Dulin <oleg.dulin@gmail.com>
2016-06-16 06:18:29 -0400

    Can remember now

--- src/myCat.js
+++ src/myCat.js
@@ -3,7 +3,15 @@
  */
 
 var request = require('request');
+var AWS=require('aws-sdk');
 
+var S3=new AWS.S3();
+
+var bucket='olegdulin-alexa-mycat';
+
+/*
+Cat fact courtesy of http://catfacts-api.appspot.com/doc.html
+ */
 var randomFact = function (callback) {
     request({
         url: 'http://catfacts-api.appspot.com/api/facts', //URL to hit
@@ -18,7 +26,65 @@ var randomFact = function (callback) {
     });
 }
 
-randomFact(function(fact) {
-   console.log(fact);
+var rememberFed = function(userId, time, callback) {
+    var remember= {
+        lastFed: time
+    };
+    S3.putObject({
+            Bucket: bucket,
+            Key: userId + "/default/food.json",
+            ContentType: "application/json",
+            Body: new Buffer(JSON.stringify(remember))
+        },
+        function(err, data) {
+            callback(err, data);
+        }
+    );
+
+}
+
+var lastFed = function(userId, callback) {
+    S3.getObject({
+            Bucket: bucket,
+            Key: userId + "/default/food.json"
+        },
+        function(err, data) {
+            if (!err) {
+                if (data!=null) {
+                    if (data.Body!=null) {
+                        callback(null, JSON.parse(data.Body.toString()));
+                    }
+                }
+                else
+                {
+                    callback(null, { lastFed : 0});
+                }
+            }
+            else
+            {
+                if (err.code=='NoSuchKey') {
+                    callback(null, { lastFed : 0});
+                }
+                else
+                {
+                    callback(err, data);
+                }
+
+            }
+
+        }
+    );
+}
+
+//randomFact(function(fact) {
+//   console.log(fact);
+//});
+
+//rememberFed("testUser", new Date().getTime(), function(err, data) {
+//   console.log(data);
+//});
+
+lastFed("testUser1", function(err, data) {
+    console.log(data);
 });
 
